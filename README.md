2f0f11fa8710cbc0e2cd6f21363c23efb26970ad
newms34 <newms3450@gmail.com>
2016-07-24 17:59:01 -0400

    blanked stuff, edited readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# Game of Life in JS
+# Conway's Game of Life (in JavaScript)
+##Description:
+This is an exercise for a local MEAN-stack meetup group. It's originally forked from [Fullstack Academy](https://github.com/FullstackAcademy), so they deserve all the credit. Note that while this simulation is done with a prototypical inheritance model, there are [many](http://jsfiddle.net/tchatel/H2y5r/) [other](http://damiankao.github.io/life/) [ways](http://disruptive-communications.com/conwaylifejavascript/) to do this!
 
-Fullstack Academy workshop.
 
+##About Conway's Game of Life
+Conway's Game of Life is a cellular automation that can produce some really cool effects. Basically, you've got a grid of cells, where each cell can either be 'alive' or 'dead' (or be 'born' or 'die'). The status of each cell is determined by how many neighbors it has:
+
+ - Cells with 2 or fewer neighbors die, as if by loneliness. Sad.
+ - Cells with 2 or 3 neighbors live (if already alive).
+ - Cells with 4 or more neighbors die (if currently alive), because overpopulation sucks.
+ - Cells with exactly 3 neighbors, if currently dead, are 'born'.
+
+This cycle basically repeats for as long as you run the simulation.
+
+##Credits:
+- I owe most of this to the brilliant folks over at [Fullstack Academy](https://github.com/FullstackAcademy).
+- A few small changes (mostly, deleting stuff) were done by me, [David Newman](https://github.com/Newms34)
--- game.js
+++ game.js
@@ -1,263 +1,89 @@
+function Conway(width, height) {
+    //here's your board constructor. What's it gonna need? For starters, you'll probably need the board height and width
+    //remember to use "this.myVar" to make the vars public!
+    //this.width = width;
 
-function GameOfLife(width,height) {
-  this.width = width;
-  this.height = height;
-  this.boardCellArr = [];
-  this.cellStatus=[];
-  this.cellSinceDeath=[];
-  this.Running = false;
-  this.timerFunc;
-  this.spd=300;
-  this.loadFileLength;
-  this.loadFile='';
-  this.loadFileSkip;
 }
 
-GameOfLife.prototype.createAndShowBoard = function () {
-  // create <table> element
-  document.getElementById('board').innerHTML = '';
-  this.cellStatus = [];
-  this.boardCellArr = [];
-  var goltable = document.createElement("tbody");
-  
-  // build Table HTML
-  var tablehtml = '';
-  for (var h=0; h<this.height; h++) {
-    tablehtml += "<tr id='row+" + h + "'>";
-    for (var w=0; w<this.width; w++) {
-      tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
-    }
-    tablehtml += "</tr>";
-  }
-  goltable.innerHTML = tablehtml;
-  
-  // add table to the #board element
-  var board = document.getElementById('board');
-  board.appendChild(goltable);
-  
-  // once html elements are added to the page, attach events to them
-  this.setupBoardEvents();
+Conway.prototype.createAndShowBoard = function() {
+    var board = document.querySelector('#board');
+    // clear the board (a table element)
+    board.innerHTML = '';
+
+    //don't forget to clear the previous data from your Conway instance. 
+    //Note that this doesn't mean you've gotta delete that instance!
+    var simtable = document.createElement("tbody");
+    //simtable - the newest in a long line of really unnecessary simulation games from Maxis™?
+
+    // build Table HTML. A simple bunch of loops will work here.
+    // Or, if you're feelin adventurous, feel free to use some angular!
+
+    simtable.innerHTML = '';
+
+    // add table to the #board element
+    board.appendChild(simtable);
+
+    //we've made our board. Now we need to make it interactive!
+    this.setupBoardEvents();
 };
 
-GameOfLife.prototype.setupBoardEvents = function() {
-  // each board cell has an CSS id in the format of: "x-y" 
-  // where x is the x-coordinate and y the y-coordinate
-  // use this fact to loop through all the ids and assign
-  // them "on-click" events that allow a user to click on 
-  // cells to setup the initial state of the game
-  // before clicking "Step" or "Auto-Play"
-  
-  // clicking on a cell should toggle the cell between "alive" & "dead"
-  // for ex: an "alive" cell be colored "blue", a dead cell could stay white
-  
-  // EXAMPLE FOR ONE CELL
-  // Here is how we would catch a click event on just the 0-0 cell
-  // You need to add the click event on EVERY cell on the board
-  
+Conway.prototype.setupBoardEvents = function() {
+    /* setup the board. Among other things, you're gonna wanna determine:
+    1) How you reference each cell
+    2) How you toggle a cell
+    */
 
-  var onCellClick = function (e) {
-    // coordinates of cell, in case you need them
-    document.getElementById('clearBt').disabled=false;
-    var coord_array = this.id.split('-');
-    var coord_hash = {x: coord_array[0], y: coord_array[1]};
-    
-    var test=coord_array.join('-');
-    var pos = gol.boardCellArr.indexOf(coord_array.join('-'));
+    var onCellClick = function(e) {
+        // How can we toggle the status of each cell when it's clicked?
+    };
 
-    // how to set the style of the cell when it's clicked
-    if (this.getAttribute('data-status') == 'dead') {
-      this.className = "alive";
-      this.setAttribute('data-status', 'alive');
-      gol.cellStatus[pos]=1;
-    } else {
-      this.className = "dead";
-      this.setAttribute('data-status', 'dead');
-      gol.cellStatus[pos]=0;
+
+    for (var i = 0, len = this.boardCellArr.length; i < len; i++) {
+        //apply that onCellClick to each cell! Note: Make SURE the onclicks are DIFFERENT, so clicking on cell (0,0) doesn't toggle cell (3,4)!
     }
-  };
-
-
- for (var x = 0; x < this.width; x++) {
-   for (var y=0; y < this.height; y++) {
-     this.boardCellArr.push(document.getElementById(x + "-" + y).id);
-     this.cellStatus.push(0);
-     this.cellSinceDeath.push(50);
-   }
- }
-
- for (var i = 0, len = this.boardCellArr.length; i < len; i++) {
-   document.getElementById(this.boardCellArr[i]).onclick = onCellClick;
- }
-
-  
+    //don't forget to apply event listeners to your buttons!
+    //document.querySelector('startBtn').onclick=someFunction();
 };
 
-GameOfLife.prototype.step = function () {
-  // Here is where you want to loop through all the cells
-  // on the board and determine, based on it's neighbors,
-  // whether the cell should be dead or alive in the next
-  // evolution of the game
-  for (var n=0;n<this.boardCellArr.length;n++){
-    var liveness=0;
-    for (var x=-1;x<=1;x++){
-      for(var y=-1;y<=1;y++){
-        //do columns first, then rows.
-        if (x==0 && y ==0){
-          //same cell: do nothing
-        }
-        else{
-          var currCellArr= this.boardCellArr[n].split('-');
-          currCellArr[0] = parseInt(currCellArr[0])+x;
-          currCellArr[1] = parseInt(currCellArr[1])+y;
-          var cellToCheck = currCellArr[0]  + '-' + currCellArr[1] ;
-          if (document.getElementById(cellToCheck) && document.getElementById(cellToCheck).className == 'alive'){
-            liveness++;
-          }
-        }
-      }
-    }
-    //check to see if cell gets to live or not
-    if (this.cellStatus[n]==1 && (liveness <2 || liveness >3)){
-      this.cellStatus[n]=0;
-    }
-    else if (this.cellStatus[n]==0 && liveness ==3){
-      this.cellStatus[n] = 1;
-    }
-    else {
-      this.cellStatus[n] = this.cellStatus[n];
-    }
-
-  }
-  for (var u=0;u<this.cellStatus.length;u++){
-    var cellToChange;
-    var lum = Math.max(0,(50-this.cellSinceDeath[u])-5);
-    var hue = Math.max(0,(-1.5 *this.cellSinceDeath[u])+60);
-    if (this.cellStatus[u]==1){
-      cellToChange = document.getElementById(this.boardCellArr[u]);
-      cellToChange.setAttribute('data-status','alive');
-      cellToChange.className = 'alive';
-      this.cellSinceDeath[u]=0;
-      
-      //cellToChange.style.backgroundColor='#fff';
-    }
-    else {
-      cellToChange = document.getElementById(this.boardCellArr[u]);
-      cellToChange.setAttribute('data-status','dead');
-      cellToChange.className = 'dead';
-      if (this.cellSinceDeath[u]<100) {
-        this.cellSinceDeath[u]++;
-      }
-      cellToChange.style.backgroundColor='hsl('+hue+',100%,'+lum+'%)';
-    }
-  }
-  
+Conway.prototype.step = function() {
+    /*In the method, you're gonna need to determine what happens each time the board 'cycles'
+    Each cycle will need to determine which cells live and which die.
+    Rules:
+     - Cells with 2 or fewer neighbors die (underpopulation)
+     - Cells with 2 or 3 neighbors lives (if already alive)
+     - Cells with more than 3 neighbors die (overpopulation)
+     - Cells with exactly 3 neighbors that are currently dead are 'reborn'
+    How can you search for an individual cell? Remember that DOM elements are 
+    NOT the same as the objects created by the Conway Constructor, so you'll need to do some associations
+    */
 };
 
-GameOfLife.prototype.enableAutoPlay = function () {
-  // Start Auto-Play by running the 'step' function
-  // automatically repeatedly every fixed time interval
-  if (this.Running){
-    //game already running, turn off!
-    document.getElementById('playBt').innerHTML = 'Play';
-    this.Running = false;
-    clearInterval(this.timerFunc);
-  }
-  else {
-    //not already running: start the 'game'!
-    document.getElementById('playBt').innerHTML = 'Pause';
-    this.Running = true;
-    this.timerFunc = setInterval(function(){gol.step()},300);
-  }
+Conway.prototype.enableAutoPlay = function() {
+    // Start Auto-Play by running the 'step' function
+    // automatically repeatedly every fixed time interval
+    // If you want, feel free to set diffent 'speeds' for the autoplay
 };
 
-GameOfLife.prototype.clearMe = function() {
-  for (var n=0;n<this.cellStatus.length;n++){
-    this.cellStatus[n] ==0;
-    var cellToChange = document.getElementById(this.boardCellArr[n]);
-    cellToChange.setAttribute('data-status','dead');
-    cellToChange.className = 'dead';
-    clearInterval(this.timerFunc);
-    document.getElementById('clearBt').disabled = true;
-  }
+Conway.prototype.clearMe = function() {
+    //a way to clear the board!
 };
 
-GameOfLife.prototype.randBoard = function() {
-  for (var n=0;n<this.cellStatus.length;n++){
-    var onOrOff= Math.floor(Math.random()*5);
-    if (!onOrOff){
-      this.cellStatus[n] ==1;
-      var cellToChange = document.getElementById(this.boardCellArr[n]);
-      cellToChange.setAttribute('data-status','alive');
-      cellToChange.className = 'alive';
-    } 
-    else {
-      this.cellStatus[n] ==0;
-      var cellToChange = document.getElementById(this.boardCellArr[n]);
-      cellToChange.setAttribute('data-status','dead');
-      cellToChange.className = 'dead';
-    }
-  }
+Conway.prototype.randBoard = function() {
+    //given a board of a certain size, randomly turn on and off all cells.
+    //NOTE: due to the rules for when a cell is alive or dead, you're 
+    //probably NOT going to want to give these cells a 50/50 chance (since
+    //that would be way overcrowded!)
 };
 
-GameOfLife.prototype.changeSpeed = function() {
-  if (this.spd == 300){
-    this.spd = 200;
-    document.getElementById('playSpd').innerHTML = 'Switch to Fast';
-  }
-  else if (this.spd == 200){
-    document.getElementById('playSpd').innerHTML = 'Switch to Slow';
-    this.spd = 100;
-  }
-  else {
-    document.getElementById('playSpd').innerHTML = 'Switch to Medium';
-    this.spd = 300;
-  }
-    clearInterval(this.timerFunc);
-    this.timerFunc = setInterval(function(){gol.step()},this.spd);
-    
+Conway.prototype.changeSpeed = function() {
+    //Optional function: change how fast 'autoplay' runs.
+
 };
 
-GameOfLife.prototype.loadPreset = function(){
-this.clearMe();
-document.getElementById('clearBt').disabled=false;
-this.loadFile = document.getElementById('loadTarg').value.split('|');
-this.loadFileLength = this.loadFile[0].length;
-if (this.loadFileLength<=this.width && this.loadFile.length<= this.height){
-  this.loadFileSkip = this.width-this.loadFileLength;//this will be the number to skip in cellStatus
-  var currCellNum=0;
-  for (var y=0;y<this.loadFile.length;y++){
-    for (var x=0;x<this.loadFileLength;x++){
-      if(this.loadFile[y][x] == 'O'){
-        //cell LIVES!
-        this.cellStatus[currCellNum] =1;
-        var cellToChange = document.getElementById(this.boardCellArr[currCellNum]);
-        cellToChange.setAttribute('data-status','alive');
-        cellToChange.className = 'alive';
-      }
-      else {
-        this.cellStatus[currCellNum] =0;
-        var cellToChange = document.getElementById(this.boardCellArr[currCellNum]);
-        cellToChange.setAttribute('data-status','dead');
-        cellToChange.className = 'dead';
-      }
-      currCellNum++;
-    }
-    currCellNum += this.loadFileSkip;
-  }
-  console.log(this.cellStatus);
-}
-else {
-  alert ('The board is too small for your pattern!');
-}
-};
-
-var gol;
+var sim; //one instance of our board constructor!
 
 
-function makeNew(){
-  var x = parseInt(document.getElementById('xVal').value);
-  var y = parseInt(document.getElementById('yVal').value);
-  gol = new GameOfLife(x,y)
-  gol.createAndShowBoard();
-  document.getElementById('gameTi').innerHTML = 'Game of Life ('+x+' by '+y+')';
+function makeNew() {
+    //make a new instance of the Conway constructor, and run all of our
+    //setup functions.
 }
